25ea15d25f41fdab592a9d865dead0135486c25f
Asif <58527783+Asif-102@users.noreply.github.com>
2022-07-27 01:53:55 +0600

    insertion sort

---
+++ insertion_sort.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printArray(int array[], int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
+
+int main(void)
+{
+    int size;
+    cin >> size;
+
+    int array[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        cin >> array[i];
+    }
+
+    cout << "Before Sort: ";
+    printArray(array, size);
+
+    // Insertion Sort Implementation
+    for(int i = 1; i < size; i++)
+    {
+        int key = array[i];
+        cout << endl << "i : " << i << " key : " << key << endl;
+        int j = i - 1;
+        while(array[j] > key && j >= 0)
+        {
+            array[j+1] = array[j];
+            j--;
+        }
+        array[j+1] = key;
+        printArray(array, size);
+    }
+
+    cout << "After Sort: ";
+    printArray(array, size);
+}
