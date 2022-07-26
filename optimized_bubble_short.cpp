f7efa2921fb74d5fe4a93c30f364419a47b1c8d8
Asif <58527783+Asif-102@users.noreply.github.com>
2022-07-27 01:42:31 +0600

    optimized bubble short

---
+++ optimized_bubble_short.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printArray(int array[], int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
+
+int main(void)
+{
+    int size;
+    cin >> size;
+
+    int array[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        cin >> array[i];
+    }
+
+    cout << "Before Sort: ";
+    printArray(array, size);
+
+    // Bubble Sort Implementation
+    for(int i=1; i < size; i++)
+    {
+        int flag = 0;
+        cout << "iteration no : " << i << endl;
+        for(int j = 0; j < size - i; j++)
+        {
+            if(array[j] > array[j+1])
+            {
+                int temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+
+                flag = 1;
+            }
+            printArray(array, size);
+        }
+        cout << endl;
+        if(!flag) break;
+    }
+
+    cout << "After Sort: ";
+    printArray(array, size);
+}
